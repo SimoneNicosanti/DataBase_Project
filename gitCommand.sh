1b83aa3b2b55281ca798d6713824841e70ed58f5
Simone <snicosanti@gmail.com>
2022-02-21 12:09:27 +0100

    ./gitCommand.sh

--- gitCommand.sh
+++ gitCommand.sh
@@ -1,8 +1,6 @@
 ##### Script Shell Per Commit Automatico #####
 # @Param $0 : messaggio del commit
 
-git add .
-git commit -m "$0"
-git push origin main
-SimoneNicosanti
-$1
+git add . ;
+git commit -m "$0" ;
+git push origin main ;
