4ed8ee1fcf84417a21238810cde1f2012ceca1eb
Simone <snicosanti@gmail.com>
2022-04-22 22:26:22 +0200

    ciao

--- gitCommand.sh
+++ gitCommand.sh
@@ -3,6 +3,4 @@
  
 git add . ;
 git commit -m "$*" ;
-git push origin main ;
-echo "snicosanti@gmail.com" ;
-echo "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" ;
+git push https://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@github.com/SimoneNicosanti/DataBase_Project.git origin main ;
