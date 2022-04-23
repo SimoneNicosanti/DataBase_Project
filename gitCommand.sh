16aa50932261d011324ed426dc7847ea1ab50292
EdmondDantes <44582979+SimoneNicosanti@users.noreply.github.com>
2022-04-23 08:31:44 +0200

    Delete gitCommand.sh

--- gitCommand.sh
+++
@@ -1,8 +0,0 @@
-##### Shell Script for Authomatic Commit #####
-# @Param $* -> All strings after ./gitCommand.sh
- 
-git add . ;
-git commit -m "$*" ;
-git push https://SimoneNicosanti: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@github.com/SimoneNicosanti/DataBase_Project.git ;
-
-# origin main
