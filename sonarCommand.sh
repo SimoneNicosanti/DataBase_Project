90567a4e47b056fa7f453fdef7cf0fede9c2f7b8
EdmondDantes <44582979+SimoneNicosanti@users.noreply.github.com>
2022-04-26 13:50:31 +0200

    Delete sonarCommand.sh

--- sonarCommand.sh
+++
@@ -1,4 +0,0 @@
-export SONAR_TOKEN=9c150853ff0987845e8fc6654bc759ad27f94e9b
-make sonarCompile
-/home/simone/SonarScanner/bin/sonar-scanner -Dsonar.organization=simonenicosanti -Dsonar.projectKey=SimoneNicosanti_DataBase_Project -Dsonar.sources=. -Dsonar.cfamily.build-wrapper-output=bw-output -Dsonar.host.url=https://sonarcloud.io  
-
